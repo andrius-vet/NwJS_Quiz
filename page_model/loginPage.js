d186419c999973e0a142205626e64ad65fbb33e0
Andrius <andrius.vetrenka@telesoftas.com>
2021-04-15 11:29:55 +0300

    add login functionality

--- page_model/loginPage.js
+++ page_model/loginPage.js
@@ -8,6 +8,21 @@ module.exports = {
         passField: "input[name='password']",
         submitButton: "button[type='submit']",
     },
-    comands: [{}]
+    commands: [{
+        loginToQuiz(browser){
+            const landingPage =  this.api.page.landingPage();
+            const menuPage =  this.api.page.menuPage();
+            return this
+                .waitForElementVisible(landingPage.elements.emailButton)
+                .waitForElementPresent(landingPage.elements.emailButton)
+                .click(landingPage.elements.emailButton)
+                .waitForElementVisible("@signInHeader")
+                .setValue("@emailField", LoginInfo.LoginName)
+                .setValue("@passField", LoginInfo.LoginPassword)
+                .click("@submitButton")
+                .waitForElementVisible(menuPage.elements.mainMenu);
+                
+        }
+    }]
 
 }
