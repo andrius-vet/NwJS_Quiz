210116edb709af7e85c9dfd767d95a752e7e5aae
Andrius <andrius.vetrenka@telesoftas.com>
2021-04-20 09:47:36 +0300

    new git error loging function

--- e2e_tests/playQuizPage.js
+++ e2e_tests/playQuizPage.js
@@ -1,13 +1,14 @@
 const mainPage = require("../page_model/mainPage.js");
+// import { getConsoleError } from "../Tools/generalFunctions.js";
 
 require("../config.js");
 
 describe("Quizmart dev login tests", function () {
-    before(function (browser) {
-        const loginPage = browser.page.loginPage();
-        browser.url(browser.launch_url);
-        loginPage.loginToQuiz();
-    });
+  before(function (browser) {
+    const loginPage = browser.page.loginPage();
+    browser.url(browser.launch_url);
+    loginPage.loginToQuiz();
+  });
 
   test("Quizmart login with email test", function (browser) {
     const mainPage = browser.page.mainPage();
@@ -26,33 +27,17 @@ describe("Quizmart dev login tests", function () {
       .assert.visible("@libraryMainButton")
       .click("@dicoveryButton")
       .click("@filtersButton")
-      .setValue('input[name="tags"]', 'test')
+      .setValue('input[name="tags"]', "test")
       .pause(1000);
-
-    async function getConsoleError() {
-      return new Promise((resolve, reject) => {
-          browser.getLogTypes()
-          .getLog('browser', function(logEntriesArray) {
-            logEntriesArray.forEach(function(log) {
-                if(log.message.includes("current-password")){
-                  console.log('[' + log.level + '] ' + log.timestamp + ' : ' + log.message);
-                  resolve(log);
-                }
-            })
-           
-          })
-      });
-    };
-    getConsoleError().then((log) => {
-        var text =log.message;
-        console.log(text);
-        browser.assert.ok(text.includes("current-password"));
-      console.log(text.includes("current-password"))
-      
-    })
-
-    
   });
 
-    after((browser) => browser.end());
+  after((browser) =>
+    browser
+      .errorLoging((log) => {
+        var text = log.message;
+        console.log(text);
+        browser.assert.ok(text.includes("current-password"));
+      })
+      .end()
+  );
 });
